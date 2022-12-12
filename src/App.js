be6515a9cce4c3840c93433f023dd325602a87b7
ThomassMathisen <thomasmathisen@hotmail.com>
2022-12-12 18:05:46 +0100

    Setup darkmode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
+import "./styles/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div className="App">
+    <div className={darkMode ? "app dark" : "app"}>
       <Router>
         <Routes>
           <Route path="/">
